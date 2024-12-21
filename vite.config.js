bd610ce2c0f0d7fe0884e4623e7019f8ec8449da
Simon Gattner <github@0x38.de>
2024-12-21 21:17:32 +0100

    chore(markup): use placeholder for title and description

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import { compileLitTemplates } from '@lit-labs/compiler';
 import { resolve } from 'path';
 const __dirname = new URL('.', import.meta.url).pathname;
 
+const transformHtmlPlugin = (data) => ({
+  name: 'transform-html',
+  transformIndexHtml: {
+    enforce: 'pre',
+    transform(html) {
+      return html.replace(/<%=\s*(\w+)\s*%>/gi, (match, p1) => data[p1] || '');
+    },
+  },
+});
+
 export default defineConfig(({ command }) => {
   return {
     root: 'src',
@@ -15,6 +25,12 @@ export default defineConfig(({ command }) => {
           before: [command === 's' && compileLitTemplates()],
           after: [],
         },
+      }),
+      transformHtmlPlugin({
+        title: 'My LinkedIn Banner',
+        description:
+          'Get your personalized LinkedIn banner with your favorite engineering SVG logos',
+        disclaimer: 'This tool is not affiliated with LinkedIn &trade;',
       }),
     ],
     build: {
