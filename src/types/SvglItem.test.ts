85e43b0934530fe83cc017cb0fc74adb1bdcd788
Simon Gattner <github@0x38.de>
2026-01-08 23:27:29 +0100

    test(svg): items use random category names

--- src/types/SvglItem.test.ts
+++ src/types/SvglItem.test.ts
@@ -5,7 +5,7 @@ describe('isSvglItem', () => {
   it('should return true for a valid SvglItem with basic properties', () => {
     const validItem: SvglItem = {
       title: 'Example Title',
-      category: 'All',
+      category: 'Design',
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
     };
@@ -16,7 +16,7 @@ describe('isSvglItem', () => {
   it('should return true for a valid SvglItem with optional wordmark properties', () => {
     const validItemWithWordmark: SvglItem = {
       title: 'Example Title',
-      category: 'All',
+      category: 'Marketplace',
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
       wordmark: { dark: 'dark-wordmark.svg', light: 'light-wordmark.svg' },
@@ -27,7 +27,7 @@ describe('isSvglItem', () => {
 
   it('should return false for an object missing required properties', () => {
     const invalidItem = {
-      category: 'All',
+      category: 'Payment',
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
     };
@@ -38,7 +38,7 @@ describe('isSvglItem', () => {
   it('should return false if the title is not a string', () => {
     const invalidItem = {
       title: 123,
-      category: 'All',
+      category: 'Analytics',
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
     };
@@ -49,7 +49,7 @@ describe('isSvglItem', () => {
   it('should return true for a category that is an array', () => {
     const validItemWithArrayCategory: SvglItem = {
       title: 'Example Title',
-      category: ['All', 'CMS'],
+      category: ['Privacy', 'CMS'],
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
     };
@@ -60,7 +60,7 @@ describe('isSvglItem', () => {
   it('should return false for a route that is null', () => {
     const invalidItem = {
       title: 'Example Title',
-      category: 'All',
+      category: 'Library',
       route: null,
       url: 'https://example.com',
     };
@@ -71,7 +71,7 @@ describe('isSvglItem', () => {
   it('should return false if wordmark is an invalid type', () => {
     const invalidItem = {
       title: 'Example Title',
-      category: 'All',
+      category: 'IoT',
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
       wordmark: 123,
@@ -91,7 +91,7 @@ describe('isSvglItem', () => {
   it('should return false for a valid object missing the url property', () => {
     const invalidItem = {
       title: 'Example Title',
-      category: 'All',
+      category: 'Database',
       route: { dark: 'dark.svg', light: 'light.svg' },
     };
 
@@ -101,7 +101,7 @@ describe('isSvglItem', () => {
   it('should return true for valid object with nested optional wordmark properties', () => {
     const validItemWithNestedWordmark: SvglItem = {
       title: 'Example Title',
-      category: 'All',
+      category: 'Framework',
       route: { dark: 'dark.svg', light: 'light.svg' },
       url: 'https://example.com',
       wordmark: { dark: 'dark-wordmark.svg', light: 'light-wordmark.svg' },
